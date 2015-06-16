ae86a2d06be9fe37b5a92093b742831b62d7088e
Raphael Rouvinov-Kats <coolcade283@gmail.com>
2015-06-16 17:38:16 -0500

    Update white_hole2.js

--- animations/spiraly/black_hole/white_hole2.js
+++ animations/spiraly/black_hole/white_hole2.js
@@ -1,1 +1,18 @@
 // https://www.khanacademy.org/computer-programming/white-hole/5907573677490176
+
+var step = 1;
+var maxLen = 280;
+var rotateStep = 1;
+
+var fullArc = 360;
+frameRate(0);
+translate(200, 200);
+var draw = function() {
+    var f = frameCount;
+    
+    rotate(rotateStep*(f%(fullArc/rotateStep)));
+    for(var i = 0; i < maxLen; i+=step) {
+        rotate(step);
+        point(10+i, 0);
+    }
+};
