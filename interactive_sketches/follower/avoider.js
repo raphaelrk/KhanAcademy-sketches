6c2b84575b63daf7b24d2f088066c29bcca7fb93
Raphael Rouvinov-Kats <coolcade283@gmail.com>
2015-06-16 19:39:47 -0500

    Create avoider.js

---
+++ interactive_sketches/follower/avoider.js
@@ -0,0 +1,28 @@
+// https://www.khanacademy.org/computer-programming/avoider/5865436553936896
+
+var draw = function() {
+    background(218, 240, 216);
+    for(var y = 20; y < 400; y+= 40) {
+        for(var x = 20; x < 400; x += 40) {
+            pushMatrix();
+            translate(x, y);
+            
+            var xdiff = mouseX - x;
+            var ydiff = mouseY - y;
+            
+            var dot = xdiff;
+            var mags = dist(0, 0, xdiff, ydiff);
+            
+            var angle = acos(dot/mags);
+            
+            if(ydiff < 0) {
+                angle = 1 - angle;
+            }
+            
+            rotate(angle);
+            strokeWeight(1 + y/100);
+            line(-(1 + y/5), 0, -(1 + y/5), 0);
+            popMatrix();
+        }
+    }
+};
