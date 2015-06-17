2f9d052565f8df8d546c8156ccca479697a80518
Raphael Rouvinov-Kats <coolcade283@gmail.com>
2015-06-16 21:06:30 -0500

    Create pivot_follow.js

---
+++ interactive_sketches/follower/fixed_follower/pivot_follow.js
@@ -0,0 +1,28 @@
+// https://www.khanacademy.org/computer-programming/pivot-follow/5785419820040192
+
+var draw = function() {
+    background(218, 240, 216);
+    for(var y = 20; y < 400; y+= 40) {
+        for(var x = 20; x < 400; x += 40) {
+            pushMatrix();
+            translate(x, y);
+            
+            var xdiff = mouseX - x;
+            var ydiff = mouseY - y;
+            
+            var dot = xdiff;
+            var mags = dist(0, 0, xdiff, ydiff);
+            
+            var angle = acos(dot/mags);
+            
+            if(ydiff < 0) {
+                angle = 1 - angle;
+            }
+            
+            rotate(angle);
+            strokeWeight(3);
+            line(-10, 0, 10, 0);
+            popMatrix();
+        }
+    }
+};
